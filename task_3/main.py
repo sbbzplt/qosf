737419afa87c0e0fa8485d98be88cb9b68cf2006
Saba <saba.bozpolat@gmail.com>
2021-01-31 14:51:22 +0300

    feat: implement get_operator function.
    	Some explanations:
    	I don't like to use an if statement to distinguish controlled gates from single qubit gates. I don't think this is an elegant way.
    	But I cannot come up with a simple yet brighter idea at the moment.
    
    	P.S: Do the following when you have time.
    
    	To improve further see: https://github.com/quantastica/qosf-mentorship/blob/master/qosf-simulator-task-additional-info.pdf

--- task_3/main.py
+++ task_3/main.py
@@ -8,11 +8,38 @@ def get_ground_state(num_qubits):
 
 
 def get_operator(total_qubits, gate_unitary, target_qubits):
-    # return unitary operator of size 2**n x 2**n for given gate and target qubits
+    gates = {
+        "i": np.identity(2),
+        "x": np.array([[0, 1], [1, 0]]),
+        "y": np.array([[0, -1.j], [1.j, 0]]),
+        "z": np.array([[1, 0], [0, -1]]),
+        "h": 1. / np.sqrt(2) * np.array([[1, 1], [1, -1]]),
+        "c0": np.array([[1, 0], [0, 0]]),
+        "c1": np.array([[0, 0], [0, 1]])
+    }
 
+    if gate_unitary.startswith("c"):
+        moment = [[gates["i"] for _ in range(2)] for _ in range(total_qubits)]
+        moment[target_qubits[0]][0] = gates["c0"]
+        moment[target_qubits[0]][1] = gates["c1"]
+        moment[target_qubits[1]][1] = gates[gate_unitary[1]]
 
-    # TODO: replace return state with actual operator.
-    return np.identity(2 ** total_qubits)
+        operator = [1., 1.]
+        for second in moment:
+            for i in range(len(second)):
+                operator[i] = np.kron(operator[i], second[i])
+        operator = operator[0] + operator[1]
+
+    else:
+        moment = [gates["i"] for _ in range(total_qubits)]
+        for i, qubit in enumerate(target_qubits):
+            moment[qubit] = gates[gate_unitary[i]]
+
+        operator = [1.]
+        for second in moment:
+            operator = np.kron(operator, second)
+
+    return np.array(operator)
 
 
 def run_program(initial_state, program):
